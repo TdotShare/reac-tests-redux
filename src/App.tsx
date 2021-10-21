2dc641d25c3270854fe4a70df33fb09f8fa48561
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2021-10-21 20:49:37 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import { RootState } from './store/index'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment , incrementByAmount } from './store/reducer/counter'
+
+
+function App() {
+
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(10))}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
