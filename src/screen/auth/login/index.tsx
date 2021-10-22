dc2b49cdd7e8eafb048eca164c289f95e5d64994
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2021-10-22 17:31:07 +0700

    Update index.tsx

--- src/screen/auth/login/index.tsx
+++ src/screen/auth/login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
-import { routerPathProtected } from './../../../router/RouterPath';
+import { routerPathProtected } from '../../../router/RouterPath';
 
 
 function LoginScreen() {
